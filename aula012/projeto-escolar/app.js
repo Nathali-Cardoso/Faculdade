bb30fa5c2460406b353933dcd46e924548660635
Nath <nathalicardoso998@outlook.com>
2024-11-10 14:19:34 -0300

    projeto finalizado

--- aula012/projeto-escolar/app.js
+++ aula012/projeto-escolar/app.js
@@ -22,10 +22,13 @@ const bodyParserJSON = bodyParser.json()
  //import da controller do aluno
  const controllerAluno = require('./controller/controllerAluno.js')
 
+ // import da controller do professor
+ const controllerProfessor = require('./controller/controllerProfessor.js')
+
 //MVC - definição de model controller e view
 
 
-//FUNÇÕES - ALUNO
+//-- FUNÇÕES - ALUNO -- 
 
 //endpoint inserir aluno
 app.post('/aluno', cors(), bodyParserJSON, async function(request, response) {
@@ -117,7 +120,8 @@ app.delete('/aluno/:id', cors(), async function(request, response) {
     }
 })
 
-//FUNÇÕES DO PROFESSOR
+
+//-- FUNÇÕES - PROFESSOR -- 
 //endpoint inserir professor
 app.post('/professor', cors(), bodyParserJSON, async function(request, response) {
     let dados = request.body
@@ -135,6 +139,71 @@ app.post('/professor', cors(), bodyParserJSON, async function(request, response)
     }
         
 })
+
+//endpoint listar professores
+app.get('/professor', cors(), async function(request, response){
+    let dadosProfessor = await controllerProfessor.listarProfessores()
+
+    if (dadosProfessor) {
+        response.status(200)
+        response.json(dadosProfessor)
+    }
+    
+    else {
+        response.status(404)
+        response.json({message: 'Não foram encontrados registros no Banco de Dados'})
+    }
+})
+
+//endpoint buscar professor pelo ID
+app.get('/professor/:id', cors(), async function(request, response) {
+    let idProfessor = request.params.id
+
+    let dadosProfessor = await controllerProfessor.buscarProfessor(idProfessor)
+
+    if (dadosProfessor){
+        response.status(200)
+        response.json(dadosProfessor)
+    }
+    else {
+        response.status(404)
+        response.json({message: 'O ID informado não foi encontrado no Banco de Dados'})
+    }
+})
+
+//endpoint alterar dados do professor
+app.put('/professor/:id', cors(), bodyParserJSON, async function(request, response){
+    let idProfessor = request.params.id
+    let dados = request.body
+
+    let result = controllerProfessor.atualizarProfessor(dados, idProfessor)
+
+    if (result) {
+        response.status(200)
+        response.json()
+    }
+    else {
+        response.status(400)
+        response.json({message: 'Não foi possível atualizar os dados no Banco de Dados'})
+    }
+})
+
+//endpoint excluir professor
+app.delete('/professor/:id', cors(), async function(request, response) {
+    let idProfessor = request.params.id
+
+    let result = controllerProfessor.excluirProfessor(idProfessor)
+
+    if (result) {
+        response.status(200)
+        response.json()
+    }
+    else {
+        response.status(400)
+        response.json({message: 'Não foi possível excluir o registro do Banco de Dados'})
+    }
+})
+
 
 
 app.listen(8080, function(){
