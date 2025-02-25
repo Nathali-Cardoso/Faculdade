99ad00f07594316f5237b8f38d7f4f9c3f320992
Nath <nathalicardoso998@outlook.com>
2025-02-25 18:36:17 -0300

    arrumando repositorio

--- aula002/app.js
+++
@@ -1,2 +0,0 @@
-console.log('Olá, mundo!!!');
-console.log('Iniciando a aula 02 de Node.JS');
--- aula003/app.js
+++
@@ -1,9 +0,0 @@
-//VAR é utilizada para criação de variáveis de escopo global
-var nome = 'Nath';
-
-//É utilizada para criação de variáveis de escopo local
-let email = 'nath@gmail.com';
-
-//É utilizado tanto no escopo local como no escopo global,
-// geralmente para conteúdos constantes (sem alteração)
-const PI = '3,14';
